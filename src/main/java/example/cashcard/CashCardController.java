0094c0faaf9a97caa1dc49469e5096301e133771
Abdulbaki <Salausmart132@gmail.com>
2025-03-11 17:01:34 +0200

    implementing GET endpoint

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,22 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+class CashCardController {
+
+//    @GetMapping("/{requestedId}")
+//    private ResponseEntity<String> findById(){
+//        return ResponseEntity.ok("{}");
+//    }
+
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<CashCard> findById() {
+        CashCard cashCard = new CashCard(1000L, 0.0);
+        return ResponseEntity.ok(cashCard);
+    }
+}
