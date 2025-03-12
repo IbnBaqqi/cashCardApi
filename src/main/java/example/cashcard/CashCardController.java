6e1ff07929a49a8c805c27702699bac7fe8f65de
Abdulbaki <Salausmart132@gmail.com>
2025-03-12 15:39:15 +0200

    implement more GET Tests

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -2,6 +2,7 @@ package example.cashcard;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,12 @@ class CashCardController {
 //    }
 
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(1000L, 0.0);
-        return ResponseEntity.ok(cashCard);
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if (requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
