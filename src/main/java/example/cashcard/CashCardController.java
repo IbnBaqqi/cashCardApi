ff6346fab5b4c8c23ebd959d39335cb8717c891c
Abdulbaki <Salausmart132@gmail.com>
2025-03-15 02:20:11 +0200

    implementing PUT

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -54,4 +54,9 @@ class CashCardController {
                 ));
         return ResponseEntity.ok(page.getContent());
     }
+
+    @PutMapping("/{requestedId}")
+    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate) {
+        return ResponseEntity.noContent().build();
+    }
 }
